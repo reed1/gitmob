a72a264519438ab53689c567e5798eda546df9c4
Ridwan Mulyadi <ridwan.mulyadi@live.com>
2026-02-03 15:09:22 +0700

    local config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['http://10.13.13.*'],
+  allowedDevOrigins: ['http://10.13.13.*', 'gitmob.loc', 'dev.gitmob.loc'],
   distDir: process.env.GITMOB_PROD ? '.next-prod' : '.next',
 };
 
